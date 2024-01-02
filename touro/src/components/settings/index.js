35f2bdc7132c198fccd2001f61ef3e5e8e26691d
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-02 00:29:10 -0300

    fix: profile

---
+++ touro/src/components/settings/index.js
@@ -0,0 +1,35 @@
+
+export default function Settings() {
+
+
+    return (
+        <div className="ml-5 w-full border border-zinc-300 rounded-lg p-4">
+
+
+            <div className="flex items-center justify-between mb-4">
+
+
+                <div className="w-1/3 h-40 border border-zinc-300 rounded-lg mr-4 bg-zinc-50">
+
+                    <div className="flex flex-col justify-between h-40 overflow-hidden">
+
+                    </div>
+
+                </div>
+
+                <div className="w-1/3 h-40 border border-zinc-300 rounded-lg mr-4 bg-zinc-50">
+
+
+                </div>
+
+                <div className="w-1/3 h-40 border border-zinc-300 rounded-lg bg-zinc-50">
+
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
--- touro/src/pages/profile/index.js
+++ touro/src/pages/profile/index.js
@@ -1,33 +1,23 @@
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Breadcrumbs from '../../templates/breadcrumbs';
+import Settings from '../../components/settings';
 
 export default function Profile() {
 
-    var infos = [
-        {
-            title: 'Nome',
-            value: 'touratank'
-        },
-        {
-            title: 'E-mail',
-            value: 'touratank@gmail.com',
-        },
-        {
-            title: 'Criado em',
-            value: '01/01/2021 às 12:00',
-        },
-        {
-            title: 'Último acesso',
-            value: '01/01/2024 às 12:00',
-        }
+    var items = [
+        { title: "Resumo", icon: "M4.5 3.75a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V6.75a3 3 0 0 0-3-3h-15Zm4.125 3a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Zm-3.873 8.703a4.126 4.126 0 0 1 7.746 0 .75.75 0 0 1-.351.92 7.47 7.47 0 0 1-3.522.877 7.47 7.47 0 0 1-3.522-.877.75.75 0 0 1-.351-.92ZM15 8.25a.75.75 0 0 0 0 1.5h3.75a.75.75 0 0 0 0-1.5H15ZM14.25 12a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H15a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3.75a.75.75 0 0 0 0-1.5H15Z" },
+        { title: "Investimentos", icon: "M2.273 5.625A4.483 4.483 0 0 1 5.25 4.5h13.5c1.141 0 2.183.425 2.977 1.125A3 3 0 0 0 18.75 3H5.25a3 3 0 0 0-2.977 2.625ZM2.273 8.625A4.483 4.483 0 0 1 5.25 7.5h13.5c1.141 0 2.183.425 2.977 1.125A3 3 0 0 0 18.75 6H5.25a3 3 0 0 0-2.977 2.625ZM5.25 9a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h13.5a3 3 0 0 0 3-3v-6a3 3 0 0 0-3-3H15a.75.75 0 0 0-.75.75 2.25 2.25 0 0 1-4.5 0A.75.75 0 0 0 9 9H5.25Z" },
+        { title: "Favoritos", icon: "M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" },
+        { title: "Configurações", icon: "M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 0 1-.517.608 7.45 7.45 0 0 0-.478.198.798.798 0 0 1-.796-.064l-.453-.324a1.875 1.875 0 0 0-2.416.2l-.243.243a1.875 1.875 0 0 0-.2 2.416l.324.453a.798.798 0 0 1 .064.796 7.448 7.448 0 0 0-.198.478.798.798 0 0 1-.608.517l-.55.092a1.875 1.875 0 0 0-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 0 1-.064.796l-.324.453a1.875 1.875 0 0 0 .2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 0 1 .796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 0 1 .517-.608 7.52 7.52 0 0 0 .478-.198.798.798 0 0 1 .796.064l.453.324a1.875 1.875 0 0 0 2.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 0 1-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 0 0 1.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 0 1-.608-.517 7.507 7.507 0 0 0-.198-.478.798.798 0 0 1 .064-.796l.324-.453a1.875 1.875 0 0 0-.2-2.416l-.243-.243a1.875 1.875 0 0 0-2.416-.2l-.453.324a.798.798 0 0 1-.796.064 7.462 7.462 0 0 0-.478-.198.798.798 0 0 1-.517-.608l-.091-.55a1.875 1.875 0 0 0-1.85-1.566h-.344ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" },
+        { title: "Verificações", icon: "M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" },
+        { title: "Segurança", icon: "M12.516 2.17a.75.75 0 0 0-1.032 0 11.209 11.209 0 0 1-7.877 3.08.75.75 0 0 0-.722.515A12.74 12.74 0 0 0 2.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 0 0 .374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 0 0-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08Zm3.094 8.016a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" },
+        { title: "Sair", icon: "M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-4.28 9.22a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72h5.69a.75.75 0 0 0 0-1.5h-5.69l1.72-1.72a.75.75 0 0 0-1.06-1.06l-3 3Z" },
     ]
-
-    var items = ["Resumo", "Investimentos", "Favoritos", "Configurações", "Verificações", "Segurança", "Sair"]
-
 
     return (
         <div>
+
             <Header className="!pr-10" />
 
             <div className='p-10 pt-0'>
@@ -40,32 +30,48 @@ export default function Profile() {
                 <div className='pt-20 absolute left-0 top-0 w-full h-64 bg-zinc-50 rounded border-b'>
                     <div className='p-10 flex items-center'>
 
-                        <div className='w-20 h-20 rounded-full mr-5'>
-                            <img src='https://avatars.githubusercontent.com/u/7' className='w-full h-full object-cover rounded-full' />
-                        </div>
-
-                        <div>
-                            <div className='font-ibm text-xl font-medium text-zinc-600'>
-                                touratank
+                        <div className='flex items-center'>
+                            <div className='w-24 h-24 rounded-full mr-5'>
+                                <img src='https://avatars.githubusercontent.com/u/7' className='w-full h-full object-cover rounded-full' />
                             </div>
 
-                            <div className='w-24 h-7 border-2 font-medium border-zinc-600 rounded-full text-xs flex items-center justify-center cursor-pointer hover:bg-zinc-600 text-zinc-600 hover:text-white'>
-                                Editar perfil
+                            <div>
+                                <div className='flex items-center'>
+                                    <div className='font-ibm text-xl font-medium text-zinc-600'>
+                                        touratank
+                                    </div>
+
+                                    <div className='w-24 h-6 ml-4 border-2 font-medium border-zinc-600 rounded-full text-xs flex items-center justify-center cursor-pointer hover:bg-zinc-600 text-zinc-600 hover:text-white'>
+                                        Editar perfil
+                                    </div>
+                                </div>
+
+
+                                <div className='flex items-center mt-2'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-zinc-700">
+                                        <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
+                                    </svg>
+
+                                    <span className='ml-2 text-sm'>Santa Catarina</span>
+                                </div>
+
+                                <div className='flex items-center mt-2'>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-zinc-700">
+                                        <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clipRule="evenodd" />
+                                    </svg>
+
+                                    <span className='ml-2 text-sm'>Membro desde  23 de mai. de 2023</span>
+                                </div>
+
                             </div>
-
                         </div>
 
-
-                        <div className='flex'>
-
-
-
-                        </div>
                     </div>
 
                 </div>
 
-                <div className='mt-32'>
+                <div className='mt-32 flex'>
                     <div className='w-60 border border-zinc-300 rounded-lg'>
 
                         <span className='block px-4 pt-4 pb-4 mb-0 font-medium text-zinc-700 border-b border-b-zinc-300'>Menu</span>
@@ -73,25 +79,37 @@ export default function Profile() {
                         <div>
                             {
                                 items.map((item, index) => {
-                                    if(items.length - 1 == index){
-                                    return (
-                                        <div key={index} className='w-full h-12 p-2 hover:bg-zinc-50 rounded-b-lg flex items-center cursor-pointer'>
-                                            <span className="text-sm ml-3 text-zinc-600">{item}</span>
-                                        </div>
-                                    )
-                                    } else{
+                                    if (items.length - 1 == index) {
                                         return (
-                                            <div key={index} className='w-full h-12 p-2 border-b border-zinc-300 hover:bg-zinc-50 flex items-center cursor-pointer'>
-                                                <span className="text-sm ml-3 text-zinc-600">{item}</span>
+                                            <div key={index} className='w-full h-12 p-2 px-4 hover:bg-zinc-50 rounded-b-lg flex items-center cursor-pointer'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-zinc-600">
+                                                    <path fillRule="evenodd" d={item.icon} />
+                                                </svg>
+
+
+                                                <span className="text-sm ml-3 text-zinc-600">{item.title}</span>
                                             </div>
                                         )
-                                    
+                                    } else {
+                                        return (
+                                            <div key={index} className='w-full h-12 p-2 px-4 border-b border-zinc-300 hover:bg-zinc-50 flex items-center cursor-pointer'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-zinc-600">
+                                                    <path fillRule="evenodd" d={item.icon} />
+                                                </svg>
+
+                                                <span className="text-sm ml-3 text-zinc-600">{item.title}</span>
+                                            </div>
+                                        )
+
                                     }
                                 })
                             }
                         </div>
 
                     </div>
+
+                    <Settings />
+
                 </div>
             </div>
 
