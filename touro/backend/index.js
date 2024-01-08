4d4c2a3e3727e553f87a776e54ec0ec8e83ecf6f
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-08 15:53:40 -0300

    fix: vercel

--- touro/backend/index.js
+++ touro/backend/index.js
@@ -8,27 +8,27 @@ app.use(express.json());
 
 const crud = require("./crud.js");
 
-app.post("/login", async (req, res) => {
+app.post("/api/login", async (req, res) => {
     res.json(await crud.login(req.params.table, req.body));
 });
 
-app.get("/:table", async (req, res) => {
+app.get("/api/:table", async (req, res) => {
     res.json(await crud.get(req.params.table));
 });
 
-app.post("/:table", async (req, res) => {
+app.post("/api/:table", async (req, res) => {
     res.json(await crud.save(req.params.table, req.body.id, req.body));
 });
 
-app.get("/:table/:id", async (req, res) => {
+app.get("/api/:table/:id", async (req, res) => {
     res.json(await crud.getID(req.params.table, req.params.id));
 });
 
-app.get("/:table/code/:code", async (req, res) => {
+app.get("/api/:table/code/:code", async (req, res) => {
     res.json(await crud.getCode(req.params.table, req.params.code));
 });
 
-app.delete("/:table/:id", async (req, res) => {
+app.delete("/api/:table/:id", async (req, res) => {
     res.json(await crud.remove(req.params.table, req.params.id));
 });
 
