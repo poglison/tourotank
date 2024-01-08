74bdba618c00b911ad9139aac15a0fd577e1a088
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-08 17:10:22 -0300

    fix: url

--- view/src/services/index.js
+++ view/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const url = 'http://localhost:8080/';
+// const url = 'http://localhost:8080/';
 
-
+const url = 'https://tourotankdb.vercel.app/';
 
 export const login = (data) => {
 
