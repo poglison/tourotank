4d6f7f44f0e61f07d9a946f2b18c13f1c849b59a
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-10 22:48:05 -0300

    fix: text-center

--- view/src/pages/hub/index.js
+++ view/src/pages/hub/index.js
@@ -6,30 +6,9 @@ export default function Hub() {
 
     const [touratanks, setTouratanks] = useState([]);
 
-    useEffect(() => {
-        const tanks = []
-
-        for (let i = 0; i < 50; i++) {
-
-            for (let j = 0; j < 50; j++) {
-                tanks.push(
-                    <span className={"absolute text-white font-montserrat font-bold opacity-5"}
-                        style={{ top: i * 20 + "px", left: j * 9 + "%", fontSize: 100 + "%" }}>
-                        tourtank</span>)
-            }
-        }
-
-        setTouratanks(tanks)
-
-
-    }, [])
-
-
-
 
     return (
         <div className="h-screen bg-primary relative overflow-hidden">
-            {touratanks}
 
 
             <div className="p-20 w-full h-full flex flex-col items-center">
