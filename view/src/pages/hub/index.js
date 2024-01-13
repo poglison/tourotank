d0868c5a34c1f1b637082fecb5942e177f47903b
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-12 22:36:12 -0300

    fix: responsive

--- view/src/pages/hub/index.js
+++ view/src/pages/hub/index.js
@@ -66,7 +66,6 @@ export default function Hub() {
     ]
 
 
-
     return (
         <div className="h-screen relative">
 
@@ -77,11 +76,11 @@ export default function Hub() {
 
                 <div className="w-full flex justify-evenly">
 
-                    <div className="w-1/2 flex flex-col justify-center items-center p-8 ">
+                    <div className="w-full flex flex-col justify-center items-center p-8 ">
                         <div className="flex items-center justify-center">
                             <h1 className="text-ibm text-4xl font-medium text-center mb-2 mr-3">Hub de projetos</h1>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.6} stroke="currentColor" className="w-10 h-10">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.6} stroke="currentColor" className="min-w-10 w-10 h-10">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
                             </svg>
                         </div>
@@ -93,31 +92,14 @@ export default function Hub() {
                         </div>
                     </div>
 
-                    <div className="w-1/2 flex justify-center items-center">
-
-                        <div className="cursor-pointer bg-primary p-2 text-white mr-4 rounded-full border border-primary hover:bg-white hover:text-primary transition-all duration-300">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-                                <path fillRule="evenodd" d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
-                            </svg>
-
-
-                        </div>
-
-                        <img className="rounded-xl h-full border border-zinc-300" src="https://www.startengine.com/discover/_next/image?url=https%3A%2F%2Fd19j0qt0x55bap.cloudfront.net%2Fproduction%2Fstartups%2F629aaf114ce5a82d86f2883f%2Fimages%2Fstartup_cover%2Ftombstone_psyonic-se-hero-image.jpg&w=2048&q=80" />
-
-                        <div className="cursor-pointer bg-primary p-2 text-white ml-4 rounded-full border border-primary hover:bg-white hover:text-primary transition-all duration-300">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-                                <path fillRule="evenodd" d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                            </svg>
-
-
-                        </div>
-                    </div>
+            
                 </div>
             </div>
 
-            <div className="flex justify-center items-center w-full bg-zinc-50 border-t border-b mt-8 pb-10">
-                <div className="flex flex-wrap justify-start mt-20 w-5/6 px-auto">
+            <div className="overflow-hidden flex justify-center items-center w-full bg-zinc-50 border-t border-b mt-8 pb-10 relative">
+
+                <div className="flex flex-wrap justify-start mt-20 w-5/6 px-auto z-10">
+
 
                     {projects.map((project, index) => {
                         return (
