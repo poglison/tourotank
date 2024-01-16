9327db123f67535205982c2b2ec971bc9cf4a14b
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-16 20:18:19 -0300

    fix: remove body-parser

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 8080;
 const cors = require("cors");
 const router = require("./router");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 
 app.use("/api", router);
--- backend/router.js
+++ backend/router.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
-app.use(express.json());
 
 const user = require("./api/user/user.controller");
 router.use("/user", user);
