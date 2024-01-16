492c618466b1c7a41bd0b6c78d7193eddabc71b9
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-16 20:03:11 -0300

    fix: router

--- view/src/services.js
+++ view/src/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const url = 'http://localhost:8080/api/';
+// const url = 'http://localhost:8080/api/';
 
-// const url = 'https://tourotankdb.vercel.app/';
+const url = 'https://tourotankdb.vercel.app/api/';
 
 export const auth = (data) => {
 
