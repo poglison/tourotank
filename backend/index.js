dbd4000707b7db85a522b0d8b6af4de1e1156ad1
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-17 19:48:50 -0300

    fix: cors

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,18 @@ const app = express();
 const port = 8080;
 const cors = require("cors");
 const router = require("./router");
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 
 
 app.use("/api", router);
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
