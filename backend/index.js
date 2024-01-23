2dac0435ee8b63a604fa6eb89c982161544fc534
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-01-22 21:50:38 -0300

    fix: cors

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,13 @@ app.use(cors({
     origin: 'https://tourotank.com.br',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
-}
-{
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-}
-
-)); 
+},
+    {
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type,Authorization',
+    }
+));
 app.use(express.json());
 
 
