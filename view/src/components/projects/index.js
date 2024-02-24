204c8527864d8931ba3101e85bd144aa06adeb7b
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-02-24 14:47:43 -0300

    fix: name project

--- view/src/components/projects/index.js
+++ view/src/components/projects/index.js
@@ -90,14 +90,14 @@ export default function Projects(props) {
 
             {props.hub !== false &&
                 <div className="w-full flex flex-col md:flex-row justify-center items-center mt-5">
-                    <span className="text-lg text-stone-700 dark:text-stone-300 font-ibm mr-1 text-center">Para ver mais projetos, você pode entrar no nosso</span>
+                    <span className="text-lg text-stone-700 dark:text-stone-300 font-ibm mr-1 text-center">Para ver mais anúncios, você pode entrar no nosso</span>
 
                     <Link to="/hub">
                         <div className="flex p-3 rounded-full   ease-in-out cursor-pointer font-ibm font-medium transition-all text-stone-800 hover:text-black dark:text-stone-200 dark:hover:text-stone-50 hover:underline hover:decoration-wavy decoration-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
                             </svg>
-                            <span className="ml-2">Hub de Projetos</span>
+                            <span className="ml-2">Hub de Anúncios</span>
 
                         </div>
                     </Link>
--- view/src/pages/hub/index.js
+++ view/src/pages/hub/index.js
@@ -89,7 +89,7 @@ export default function Hub() {
             <Container className="flex flex-col items-center justify-center mt-40 mb-20 text-stone-700 dark:text-stone-300">
                 <div className="w-full flex flex-col justify-center items-center">
                     <div className="flex items-center justify-center">
-                        <h1 className="text-ibm text-4xl font-medium text-center mb-2 mr-3 ">Hub de projetos</h1>
+                        <h1 className="text-ibm text-4xl font-medium text-center mb-2 mr-3 ">Hub de Anúncios</h1>
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.6} stroke="currentColor" className="min-w-10 w-10 h-10">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
