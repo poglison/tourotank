2bba944e7a6295ec5704e691ebb8f3c9b5c5eb00
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-03-06 21:32:12 -0300

    fix: banner

--- view/src/components/banner/index.js
+++ view/src/components/banner/index.js
@@ -30,17 +30,18 @@ export default function Banner() {
 
 
     return (
-        <div className="px-5 md:px-10 overflow-hidden h-96 pr-20 md:pr-10 pt-20 font-montserrat relative">
+        <div className="w-full px-5 md:px-10 overflow-hidden h-96 pr-20 md:pr-10 pt-20 font-montserrat relative">
 
-            <div className="h-full flex flex-col justify-center">
-                <h2 className="text-xl md:text-5xl font-bold text-stone-700 dark:text-stone-300 z-10 relative">a negociação se resume a dois</h2>
+            <div className="w-3/4 h-full flex flex-col justify-center xl:items-center">
+                
+                <h2 className="hidden md:flex text-xl md:text-5xl font-bold text-stone-700 dark:text-stone-300 z-10 relative">a negociação se resume a dois</h2>
 
-                <div className="flex items-center">
-                    <h1 className="text-5xl md:text-8xl font-bold text-primary z-10 relative">cliques</h1>
+                <div className="flex items-center flex-col md:flex-row">
+                    <h1 className="hidden md:flex text-5xl md:text-8xl font-bold text-primary z-10 relative">cliques</h1>
 
-                    <h1 className={"ml-4 text-5xl md:text-8xl font-bold text-primary z-10 relative transition-all " + (select == "comprar" ? "rotate-30" : "")}>👉</h1>
+                    <h1 className={"hidden md:flex ml-4 text-5xl md:text-8xl font-bold text-primary z-10 relative transition-all " + (select == "comprar" ? "rotate-30" : "")}>👉</h1>
 
-                    <div className="flex flex-col mt-4 -ml-4">
+                    <div className="hidden md:flex flex-col mt-4 -ml-4">
                         <Link to="/ad/new" onMouseEnter={() => { setSelect("anunciar") }}>
                             <p className={"transition-all ml-4 font-montserkrat text-4xl text-stone-300 hover:text-primary font-bold " + (select == "anunciar" ? "!text-primary" : "")}>anunciar</p>
                         </Link>
