5f7e8b51329bb0dd1d72c8632d534d4ff175acbd
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-03-09 23:20:45 -0300

    fix: cursor

--- view/src/components/banner/index.js
+++ view/src/components/banner/index.js
@@ -23,7 +23,7 @@ export default function Banner() {
         // "negociar",
         // "startar",
         // "investir em ideias",
-        "cliques 👉",
+        ">> cliques 👉",
     ];
 
     var text = texts[Math.floor(Math.random() * texts.length)];
@@ -32,14 +32,14 @@ export default function Banner() {
     return (
         <div className="w-full px-5 md:px-10 overflow-hidden h-96 pr-20 md:pr-10 pt-20 font-montserrat relative">
 
-            <div className="w-3/4 h-full flex flex-col justify-center xl:items-center">
+            <div className="w-3/4 h-full flex flex-col justify-center">
                 
                 <h2 className="hidden md:flex text-xl md:text-5xl font-bold text-stone-700 dark:text-stone-300 z-10 relative">a negociação se resume a dois</h2>
 
                 <div className="flex items-center flex-col md:flex-row">
                     <h1 className="hidden md:flex text-5xl md:text-8xl font-bold text-primary z-10 relative">cliques</h1>
 
-                    <h1 className={"hidden md:flex ml-4 text-5xl md:text-8xl font-bold text-primary z-10 relative transition-all " + (select == "comprar" ? "rotate-30" : "")}>👉</h1>
+                    <h1 className={"hidden md:flex ml-4 text-5xl md:text-8xl font-bold text-primary z-10 relative transition-all " + (select == "comprar" ? "rotate-30" : "")}><img className="w-24 mr-5" src="/imgs/cursor/point.png"></img></h1>
 
                     <div className="hidden md:flex flex-col mt-4 -ml-4">
                         <Link to="/ad/new" onMouseEnter={() => { setSelect("anunciar") }}>
