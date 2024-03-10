c92d517b39932916b85e06d41741a348bb0260af
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-03-10 13:34:36 -0300

    fix: select

--- view/src/pages/new_ad/index.js
+++ view/src/pages/new_ad/index.js
@@ -21,6 +21,7 @@ export default function NewAd(props) {
 
     const [ad, setAd] = useState({});
     const [type, setType] = useState(1);
+    const [close, setClose] = useState(false);
 
     useEffect(() => {
         document.scrollingElement.scrollTop = 0;
@@ -188,10 +189,14 @@ export default function NewAd(props) {
         { "label": "Outros Jogos", "value": "outros_jogos", "image": "/imgs/categories/Outros Jogos.png" },
         { "label": "Sugerir Jogo", "value": "sugerir_jogo", "image": "/imgs/bull/icone.png" }
     ];
+
+
+    const [isOpen, setIsOpen] = useState(false);
     
 
+
     return (
-        <div className="bg-white dark:bg-stone-950 overflow-x-hidden">
+        <div className="bg-white dark:bg-stone-950 overflow-x-hidden" onClick={() => { setIsOpen(false) }}>
 
             <Header />
 
@@ -217,8 +222,13 @@ export default function NewAd(props) {
 
                     </div>
 
-                </div>
+                    <div className="flex flex-col mt-5">
+                        <span className="font-ibm text-xs text-stone-600">Categoria</span>
 
+                        <Select isOpen={isOpen} setIsOpen={setIsOpen} onChange={(e) => { setAd({ ...ad, category }) }} options={categories} />
+                    </div>
+
+                </div>
 
                 <div className="w-full flex flex-col mt-6">
 
@@ -228,51 +238,49 @@ export default function NewAd(props) {
 
                 </div>
 
-                <div className="w-full flex flex-col mt-6">
-                    <span className="text-ibm text-lg font-medium mb-4 block text-stone-800 dark:text-stone-200">Informações do Anúncio</span>
 
-                    <div className="flex flex-col">
-                        <span className="font-ibm text-xs text-stone-600">Valor do anúncio</span>
+                {type == 1 || type == 3 ?
+                    <div className="w-full flex flex-col mt-6">
+                        <span className="text-ibm text-lg font-medium mb-4 block text-stone-800 dark:text-stone-200">Informações do Anúncio</span>
 
-                        <input value={ad?.price} onChange={(e) => {
-                            setAd({ ...ad, unprice: e.target.value })
-                            var value = maskCoin(e);
-                            console.log((value.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '') / 1000))
-                            var tanks = (value.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '') / 1000).toFixed(0) > 0 ? (value.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '') / 1000).toFixed(0) : 1;
+                        <div className="flex flex-col">
+                            <span className="font-ibm text-xs text-stone-600">Valor do anúncio</span>
+
+                            <input value={ad?.price} onChange={(e) => {
+                                setAd({ ...ad, unprice: e.target.value })
+                                var value = maskCoin(e);
+                                console.log((value.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '') / 1000))
+                                var tanks = (value.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '') / 1000).toFixed(0) > 0 ? (value.replaceAll('R$', '').replaceAll('.', '').replaceAll(',', '') / 1000).toFixed(0) : 1;
 
 
 
-                            setAd({ ...ad, price: value, tanks: tanks })
-                        }} className="border dark:border-stone-700 p-2 px-4 rounded-xl mt-1 dark:bg-stone-950 outline-none font-ibm text-lg text-stone-800 dark:text-stone-300 dark:placeholder:text-stone-600" />
+                                setAd({ ...ad, price: value, tanks: tanks })
+                            }} className="border dark:border-stone-700 p-2 px-4 rounded-xl mt-1 dark:bg-stone-950 outline-none font-ibm text-lg text-stone-800 dark:text-stone-300 dark:placeholder:text-stone-600" />
+                        </div>
+
+                        <div className="flex flex-col mt-5">
+                            <span className="font-ibm text-xs text-stone-600">Quantidade em estoque</span>
+
+                            <input onChange={(e) => { setAd({ ...ad, qtd: e.target.value }) }} className="border dark:border-stone-700 p-2 px-4 rounded-xl mt-1 dark:bg-stone-950 outline-none font-ibm text-lg text-stone-800 dark:text-stone-300 dark:placeholder:text-stone-600" />
+                        </div>
+                        <div className="mt-10 border dark:border-stone-700 h-36 object-cover rounded-xl flex items-center justify-center cursor-pointer hover:bg-stone-50 dark:hover:bg-stone-900 text-stone-500 text-lg relative">
+                            <input id="file" type="file" className="absolute w-full h-full hidden" onChange={(e) => { convertBase64(e) }} />
+                            <label for="file" className="cursor-pointer absolute w-full h-full flex items-center justify-center" />
+
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 mr-4">
+                                <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clipRule="evenodd" />
+                            </svg>
+                            <span>Adicionar imagem</span>
+                        </div>
                     </div>
+                    :
+                    null
+                }
 
-                    <div className="flex flex-col mt-5">
-                        <span className="font-ibm text-xs text-stone-600">Quantidade em estoque</span>
-
-                        <input onChange={(e) => { setAd({ ...ad, qtd: e.target.value }) }} className="border dark:border-stone-700 p-2 px-4 rounded-xl mt-1 dark:bg-stone-950 outline-none font-ibm text-lg text-stone-800 dark:text-stone-300 dark:placeholder:text-stone-600" />
-                    </div>
-
-                    <div className="flex flex-col mt-5">
-                        <span className="font-ibm text-xs text-stone-600">Categoria</span>
-
-                        <Select onChange={(e) => { setAd({ ...ad, category }) }} options={categories} />
-                    </div>
-
-                    <div className="mt-10 border dark:border-stone-700 h-56 object-cover rounded-xl flex items-center justify-center cursor-pointer hover:bg-stone-50 dark:hover:bg-stone-900 text-stone-500 text-lg relative">
-                        <input id="file" type="file" className="absolute w-full h-full hidden" onChange={(e) => { convertBase64(e) }} />
-                        <label for="file" className="cursor-pointer absolute w-full h-full flex items-center justify-center" />
-
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 mr-4">
-                            <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clipRule="evenodd" />
-                        </svg>
-                        <span>Adicionar imagem</span>
-                    </div>
-
-                </div>
 
             </Container>
             <Footer />
         </div>
     )
-    
+
 }
--- view/src/templates/select.js
+++ view/src/templates/select.js
@@ -1,38 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
-export default function Select (props) {
+export default function Select(props) {
+    const [options, setOptions] = useState(props.options);
     const [selectedValue, setSelectedValue] = useState("Não selecionado");
     const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        setIsOpen(props.isOpen);
+    }, [props.isOpen]);
+
+    useEffect(() => {
+        props.setIsOpen(isOpen);
+    }, [isOpen]);
 
     const handleClick = (option) => {
         setSelectedValue(option.label);
         setIsOpen(false);
     };
 
+
+    const handle = (e) => {
+        setIsOpen(true);
+        setSelectedValue(e.target.value);
+        if (e.target.value !== "") {
+            setOptions(props.options.filter(option => option.label.toLowerCase().includes(e.target.value.toLowerCase())));
+        } else {
+            setOptions(props.options);
+        }
+    }
+
     return (
-            <div className="relative">
-                <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    className="flex w-full border dark:border-stone-700 p-2 px-4 rounded-xl mt-1 dark:bg-stone-950 outline-none font-ibm text-lg text-stone-800 dark:text-stone-300 dark:placeholder:text-stone-600"
-                >
-                    {selectedValue}
-                </button>
-                {isOpen && (
-                    <div className="absolute h-72 overflow-auto z-10 w-full mt-1 bg-white dark:text-stone-300 dark:bg-stone-950 border dark:border-stone-700 rounded-lg shadow-lg">
-                        {props.options.map((option, index) => (
-                            
-                            <div
-                                key={option.label}
-                                onClick={() => handleClick(option)}
-                                className={"flex items-center cursor-pointer border-b dark:border-stone-700 p-3 px-4 hover:bg-stone-100 dark:hover:bg-stone-900 " + (index === 0 ? "rounded-t-md" : "") + (index === props.options.length - 1 ? "rounded-b-md !border-b-0" : "")}
-                            >
-                                {option.image && <img src={option.image} className="w-8 h-8 mr-2 rounded-md" />}
-                                {option.label}
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
+        <div className="relative">
+            <input
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex w-full border dark:border-stone-700 p-2 px-4 rounded-xl mt-1 dark:bg-stone-950 outline-none font-ibm text-lg text-stone-800 dark:text-stone-300 dark:placeholder:text-stone-600"
+                onChange={(e) => handle(e)}
+                value={selectedValue}
+            >
+
+            </input>
+            {isOpen && (
+                <div className="absolute h-56 overflow-auto z-10 w-full mt-1 bg-white dark:text-stone-300 dark:bg-stone-950 border dark:border-stone-700 rounded-lg shadow-lg">
+                    {options.map((option, index) => (
+
+                        <div
+                            key={option.label}
+                            onClick={() => handleClick(option)}
+                            className={"flex items-center cursor-pointer border-b dark:border-stone-700 p-3 px-4 hover:bg-stone-100 dark:hover:bg-stone-900 " + (index === 0 ? "rounded-t-md" : "") + (index === options.length - 1 ? "rounded-b-md !border-b-0" : "")}
+                        >
+                            {option.image && <img src={option.image} className="w-8 h-8 mr-2 rounded-md" />}
+                            {option.label}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
     );
 };
