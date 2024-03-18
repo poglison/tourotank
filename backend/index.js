57842b2d632d03a1a990eab110fbf3da17f3b1f7
leopoglia <92129999+leopoglia@users.noreply.github.com>
2024-03-18 19:33:24 -0300

    fix: verceç

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-// const cors = require("cors");
+const cors = require("cors");
 const router = require("./router");
 const bodyParser = require('body-parser');
 
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
-app.use();
+app.use(cors(
+
+    {
+        origin: 'https://tourotank.com.br',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type,Authorization',
+    },
+    {
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type,Authorization',
+    }
+));
 app.use(express.json());
 
 
